1585d986900a50b04fb8265960ab19f3b492ee0e
Kevin Koch <kevin.koch23@d155.org>
2022-09-23 07:28:06 -0500

    Initial Commit

---
+++ GradesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradesApp
+//
+//  Created by KEVIN KOCH on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
